bf5354526193baa6027bec20bce41cb4d466cd89
Mike Francis <mike@benchmark.co.uk>
2015-05-07 16:38:58 +0100

    Started tests.

---
+++ tests/UploadableTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use \Mockery;
+use \MikeFrancis\Uploadable\UploadableTrait;
+
+class UploadableTraitTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * Prepare tests.
+   */
+  public function setUp() {
+    $this->model = Mockery::mock('ModelStub');
+  }
+
+  /**
+   * Clean up tests.
+   */
+  public function tearDown() {
+    Mockery::close();
+  }
+
+  public function testItDoesSomething() {
+    
+  }
+
+}
+
+class ModelStub {
+
+  use UploadableTrait;
+
+  protected $uploadable = [
+    'input_1' => [],
+    'input_2' => []
+  ];
+
+}
