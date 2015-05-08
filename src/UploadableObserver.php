050801efe97fec7afb44c4b57d75449e86b9f31a
Mike Francis <mike@benchmark.co.uk>
2015-05-08 11:28:08 +0100

    Add docblocks.

--- src/UploadableObserver.php
+++ src/UploadableObserver.php
@@ -4,11 +4,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class UploadableObserver {
 
+  /**
+   * Trigger function when saving a model (creating/editing).
+   * 
+   * @param  Model  $model
+   * @return void
+   */
   public function saving(Model $model)
   {
     $model->performUploads();
   }
 
+  /**
+   * Trigger function when deleting a model.
+   * 
+   * @param  Model  $model
+   * @return void
+   */
   public function deleting(Model $model)
   {
     $model->performDeletes();
